bc826ca19b65215f2fa431f9c828c2b36b4232d7
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-11-12 16:28:01 +0000

    adding comments to the unchecked or runtime exception

--- src/com/turntablG14/Main.java
+++ src/com/turntablG14/Main.java
@@ -1,52 +1,61 @@
 package com.turntablG14;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException { //throws FileNotFoundException
 	// write your code here
         //checked exception
-       // readFile("myFile.txt");
+        //readFile("myFile.txt");
 
         //unchecked exception
-//        String name = "AKORLIE";
+//        String name = "EDWARD";
 //        printLength(null);
           addNumbers(-3,-5);
+          printLength("edwardPearlAlex");
 
-          ArrayList<Integer> myList2 = new ArrayList();
-          myList2.add(6);
-          myList2.add(8);
-          myList2.add(78);
+//          ArrayList<Integer> myList2 = new ArrayList();
+//          myList2.add(6);
+//          myList2.add(8);
+//          myList2.add(78);
+//
+//
+//          try{
+//              System.out.println(myList2.get(6));
+//          }catch (IndexOutOfBoundsException e){
+//              System.out.println("please the list has only 3 members");
+//          }
 
-
-          try{
-              System.out.println(myList2.get(6));
-          }catch (IndexOutOfBoundsException e){
-              System.out.println("please the list has only 3 members");
-          }
     }
 
 
     //checked exception
-//    public  static void readFile(String fileName) throws FileNotFoundException {//throw exception in the method signature
-//       FileReader reader = new FileReader(fileName);
-//    }
+    public  static void readFile(String fileName) throws FileNotFoundException {//throw exception in the method signature
+       FileReader reader = new FileReader(fileName);
 
+    }
+
+    //checked exception
     public static void addNumbers(Integer num1, Integer num2){
         System.out.println(num1 + num2);
     }
 
 
-    //unchecked exception
-//    public static void printLength(String myString){
-//        try {
-//            System.out.println(myString.length());
-//        }catch (NullPointerException npe){
-//            System.out.println("String can not be null");
-//        }
-//    }
+
+   //unchecked exception
+    public static void printLength(String myString){
+        try {
+            System.out.println(myString.length()); // this line of code prints the length of the input string
+        }catch (NullPointerException npe){
+            System.out.println("String can not be null");
+        }
+    }
+
+
+
 }
 
 // how java detect which part to throw exception
