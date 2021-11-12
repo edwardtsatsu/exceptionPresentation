89dda11d0ec91bb71bdda4ab2e529915bb37c53a
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-11-12 16:58:22 +0000

    adding comments to the unchecked or runtime exception

--- src/com/turntablG14/Main.java
+++ src/com/turntablG14/Main.java
@@ -3,6 +3,7 @@ package com.turntablG14;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -11,27 +12,30 @@ public class Main {
         //checked exception
         //readFile("myFile.txt");
 
+
         //unchecked exception
 //        String name = "EDWARD";
 //        printLength(null);
-          addNumbers(-3,-5);
           printLength("edwardPearlAlex");
           printLength(null);
 
           //creating and list and adding numbers to the list
 
-         // Question ?
-         // What kind of exception is this ?   A. CHECKED    B. UNCHECKED  C. NONE OF THE ABOVE
-//          ArrayList<Integer> myList2 = new ArrayList();
-//          myList2.add(6);
-//          myList2.add(8);
-//          myList2.add(78);
-//
-//          try{
-//              System.out.println(myList2.get(6));
-//          }catch (IndexOutOfBoundsException e){
-//              System.out.println("please the list has only 3 members");
-//          }
+        // !! QUESTION FOR THE DAY !!
+        // What kind of exception is this ?   A. Checked    B. UnChecked
+        // C. RunTimeException    D. CompilerTimeException  E. Both B and C are the Same and is Right   F. Both A and D are the same and is Right
+
+          List<Integer> myList2 = new ArrayList();
+          myList2.add(6);
+          myList2.add(8);
+          myList2.add(78);
+
+
+          try{
+              System.out.println(myList2.get(6));
+          }catch (IndexOutOfBoundsException e){
+              System.out.println("please the list has only 3 members");
+          }
 
     }
 
@@ -41,12 +45,6 @@ public class Main {
        FileReader reader = new FileReader(fileName);
 
     }
-
-    //checked exception
-    public static void addNumbers(Integer num1, Integer num2){
-        System.out.println(num1 + num2);
-    }
-
 
 
    //unchecked exception
@@ -58,9 +56,6 @@ public class Main {
         }
     }
 
-
-
 }
 
 // how java detect which part to throw exception
-// there are sub
