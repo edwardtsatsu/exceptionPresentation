3991805d6a2921600b417d33950468cf4cb052fe
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-11-12 17:07:58 +0000

    adding comments to the unchecked or runtime exception

--- src/com/turntablG14/Main.java
+++ src/com/turntablG14/Main.java
@@ -9,53 +9,55 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException { //throws FileNotFoundException
 	// write your code here
-        //checked exception
-        //readFile("myFile.txt");
+        //checked exception, and this happens at compiler time !!
+        readFile("myFile.txt");
 
 
         //unchecked exception
 //        String name = "EDWARD";
 //        printLength(null);
-          printLength("edwardPearlAlex");
-          printLength(null);
+//        printLength("edwardPearlAlex");
+//        printLength(null);
 
           //creating and list and adding numbers to the list
 
         // !! QUESTION FOR THE DAY !!
-        // What kind of exception is this ?   A. Checked    B. UnChecked
+        // What kind of exception is below ?   A. Checked    B. UnChecked
         // C. RunTimeException    D. CompilerTimeException  E. Both B and C are the Same and is Right   F. Both A and D are the same and is Right
 
-          List<Integer> myList2 = new ArrayList();
-          myList2.add(6);
-          myList2.add(8);
-          myList2.add(78);
-
-
-          try{
-              System.out.println(myList2.get(6));
-          }catch (IndexOutOfBoundsException e){
-              System.out.println("please the list has only 3 members");
-          }
+//          List<Integer> myNumbers = new ArrayList();
+//          myList2.add(6);
+//          myList2.add(8);
+//          myList2.add(78);
+//
+//
+//          try{
+//              System.out.println(myNumbers.get(6));
+//          }catch (IndexOutOfBoundsException e){
+//              System.out.println("please the list has only 3 members");
+//          }
 
     }
 
 
-    //checked exception
-    public  static void readFile(String fileName) throws FileNotFoundException {//throw exception in the method signature
+    //checked exception, and this happens at compiler time !!
+    public  static void readFile(String fileName) throws FileNotFoundException {// By adding throws decoration in the method signature
        FileReader reader = new FileReader(fileName);
 
     }
 
 
    //unchecked exception
-    public static void printLength(String myString){
-        try {
-            System.out.println(myString.length()); // this line of code prints the length of the input string
-        }catch (NullPointerException npe){
-            System.out.println("String can not be null");
-        }
-    }
+//    public static void printLength(String myString){
+//        try {
+//            System.out.println(myString.length()); // this line of code prints the length of the input string
+//        }catch (NullPointerException npe){
+//            System.out.println("String can not be null");
+//        }
+//    }
 
 }
 
+
 // how java detect which part to throw exception
+// NB: You can add "try catch block" to handle checked exception as you do same for unchecked exception as well !!
